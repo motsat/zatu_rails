f44b57fe24ec4ebc2897a22dc8372cf556e42306
motsat <konpeiex@gmail.com>
2017-09-07 00:31:59 +0900

    zatu push

---
+++ spec/factories/user_team.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user_team do
+     association :user
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+FactoryGirl.definition_file_paths = %w{./factories ./test/factories ./spec/factories}
+FactoryGirl.find_definitions
