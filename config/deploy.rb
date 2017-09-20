0949d68a85932e4b3ce32fceed94697c4a18addd
motsat <konpeiex@gmail.com>
2017-09-21 00:04:30 +0900

    zatu

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock "3.9.1"
 
-set :application, "my_app_name"
+set :application, "zatu_rails"
 set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/zatu_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
