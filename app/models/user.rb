00c667ed0eeb61e661a3363007e18b30283e8888
motsat <konpeiex@gmail.com>
2017-10-11 00:14:18 +0900

    elastic custom setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
 class User < ApplicationRecord
-  searchkick
-end
+  searchkick settings: {
+    index: {
+      analysis: {
+        analyzer: {
+          ja_analyzer: { type: :custom, 
+                         char_filter: [ :icu_normalizer ], # Unicode正規化ウ゛ェ → ヴェ ＡＢＣ → ABC ① → 1 ㊤ → 上 Ⅲ → III ㌶ → ヘクタールとか
+                         tokenizer: :ja_tokenizer,
+                         filter: [ :cjk_width,  # 半角・全角などの統一
+                                   :part_of_speech, 
+                                   :kuromoji_baseform, # 日本語の動詞と形容詞を原型に戻すfilter
+                                   ]
+          }
+        },
+        filter:{
+          #part_of_speech:{ type: :kuromoji_part_of_speech, stoptags: [ "副詞-助詞類接続" ] },
+          # stoptagsはデフォでいいかも（全部やってくれる？）
+          part_of_speech:{ type: :kuromoji_part_of_speech  # 品詞を除外。stoptags設定で個別に除外対象を選択できる。デフォは全て？
+          },
+          #synonym:{
+          #  type: :synonym,
+          #  synonyms_path: :synonyms.txt
+          #}
+        },
+        tokenizer: {
+          ja_tokenizer:{ type: :kuromoji_tokenizer, mode: :search, #user_dictionary: :user_dictionary.txt }
+          }
+        }
+      }
+    }
+  }
+end 
